f40927f3ae67ca1f8f6d83feceff2e84d08c8444
Tom Marks <thomas.marks@gmail.com>
2017-08-21 09:49:09 -0400

    Moves tokenize into dedicated module

--- jsonxs/jsonxs.py
+++ jsonxs/jsonxs.py
@@ -87,6 +87,8 @@ For example:
     ['fboender', 'ppeterson']
 """
 
+from __future__ import absolute_import
+from jsonxs.tokenize import tokenize
 
 ACTION_GET = 'get'
 ACTION_SET = 'set'
@@ -95,48 +97,6 @@ ACTION_APPEND = 'append'
 ACTION_INSERT = 'insert'
 ACTION_MKDICT = 'mkdict'
 ACTION_MKLIST = 'mklist'
-
-
-def tokenize(expr):
-    """
-    Parse a string expression into a set of tokens that can be used as a path
-    into a Python datastructure.
-    """
-    tokens = []
-    escape = False
-    cur_token = ''
-
-    for c in expr:
-        if escape == True:
-            cur_token += c
-            escape = False
-        else:
-            if c == '\\':
-                # Next char will be escaped
-                escape = True
-                continue
-            elif c == '[':
-                # Next token is of type index (list)
-                if len(cur_token) > 0:
-                    tokens.append(cur_token)
-                    cur_token = ''
-            elif c == ']':
-                # End of index token. Next token defaults to a key (dict)
-                if len(cur_token) > 0:
-                    tokens.append(int(cur_token))
-                    cur_token = ''
-            elif c == '.':
-                # End of key token. Next token defaults to a key (dict)
-                if len(cur_token) > 0:
-                    tokens.append(cur_token)
-                    cur_token = ''
-            else:
-                # Append char to token name
-                cur_token += c
-    if len(cur_token) > 0:
-        tokens.append(cur_token)
-
-    return tokens
 
 
 def jsonxs(data, expr, action=ACTION_GET, value=None, default=None):
